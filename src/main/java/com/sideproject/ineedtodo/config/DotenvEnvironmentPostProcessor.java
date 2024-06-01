267bf2541e1d6d08349c466f1d46a6b5ea0cb5ba
NQD190523 <duc19052003@gmail.com>
2024-06-01 09:57:19 +0700

    rebase and hide sensitive information

--- src/main/java/com/sideproject/ineedtodo/config/DotenvEnvironmentPostProcessor.java
+++ src/main/java/com/sideproject/ineedtodo/config/DotenvEnvironmentPostProcessor.java
@@ -1,4 +1,4 @@
-package com.sideproject.ineedtodo;
+package com.sideproject.ineedtodo.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
